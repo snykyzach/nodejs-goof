1cdf7fbb5043e96a9ea89685c2fbe47148d79c67
Zach J <zach.johnsotn@snyk.io>
2024-11-04 16:23:39 -0700

    added a vulnerable function (actually a copy of one) to show PR Check fail (hopefully) in source control

--- routes/index.js
+++ routes/index.js
@@ -34,6 +34,24 @@ exports.index = function (req, res, next) {
     });
 };
 
+//this function is a repeat to show PR check functionality in Snyk
+exports.loginHandler = function (req, res, next) {
+  //unsanitized user/pass being passed in nosql query
+  if (validator.isEmail(req.body.username)) {
+    User.find({ username: req.body.username, password: req.body.password }, function (err, users) {
+      if (users.length > 0) {
+        const redirectPage = req.body.redirectPage
+        const session = req.session
+        const username = req.body.username
+        return adminLoginSuccess(redirectPage, session, username, res)
+      } else {
+        return res.status(401).send()
+      }
+    });
+  } else {
+    return res.status(401).send()
+  }
+};
 exports.loginHandler = function (req, res, next) {
   if (validator.isEmail(req.body.username)) {
     User.find({ username: req.body.username, password: req.body.password }, function (err, users) {
